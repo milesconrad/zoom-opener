5378fc31034de4070ed886d042de854eecb4ea3d
mmiiles <milesconrad@pm.me>
2021-03-15 16:47:40 -0400

    moved advisory method

--- Opener.java
+++ Opener.java
@@ -2,17 +2,9 @@ import java.awt.Desktop;
 import java.net.URI;
 
 class Opener {
-    public void advisory(){
-        try {
-            URI url0 = new URI("https://oneoakland.webex.com/meet/chisnell");
-            Desktop.getDesktop().browse(url0);
-        } catch (Exception e) {
-            System.out.print("Error loading page");
-        }
-    }
     public void first(){
         try {
-            URI url1 = new URI("https://oneoakland.webex.com/meet/david.weeks");
+            URI url1 = new URI("1st Hour Link");
             Desktop.getDesktop().browse(url1);
         } catch (Exception e) {
             System.out.print("Error loading page");
@@ -20,7 +12,7 @@ class Opener {
     }
     public void second(){
         try {
-            URI url2 = new URI("https://oneoakland.webex.com/meet/kirsten.williams");
+            URI url2 = new URI("2nd Hour Link");
             Desktop.getDesktop().browse(url2);
         } catch (Exception e) {
             System.out.print("Error loading page");
@@ -28,7 +20,7 @@ class Opener {
     }
     public void third(){
         try {
-            URI url3 = new URI("https://oneoakland.webex.com/meet/mrcastle");
+            URI url3 = new URI("3rd Hour Link");
             Desktop.getDesktop().browse(url3);
         } catch (Exception e) {
             System.out.print("Error loading page");
@@ -36,7 +28,7 @@ class Opener {
     }
     public void fourth(){
         try {
-            URI url4 = new URI("https://oneoakland.webex.com/meet/alec.snyder");
+            URI url4 = new URI("4th Hour Link");
             Desktop.getDesktop().browse(url4);
         } catch (Exception e) {
             System.out.print("Error loading page");
@@ -44,7 +36,7 @@ class Opener {
     }
     public void fifth(){
         try {
-            URI url5 = new URI("https://oneoakland.webex.com/meet/kristen.elsner");
+            URI url5 = new URI("5th Hour Link");
             Desktop.getDesktop().browse(url5);
         } catch (Exception e) {
             System.out.print("Error loading page");
@@ -52,8 +44,16 @@ class Opener {
     }
     public void sixth(){
         try {
-            URI url6 = new URI("https://oneoakland.webex.com/meet/jennifer.hull");
+            URI url6 = new URI("6th Hour Link");
             Desktop.getDesktop().browse(url6);
+        } catch (Exception e) {
+            System.out.print("Error loading page");
+        }
+    }
+    public void advisory(){
+        try {
+            URI url0 = new URI("Advisory Link");
+            Desktop.getDesktop().browse(url0);
         } catch (Exception e) {
             System.out.print("Error loading page");
         }
