7b0a9aa2beb1dc7c11bede98fa67f6cb78a1c7a4
mmiiles <milesconrad@pm.me>
2021-03-15 16:44:27 -0400

    updated for schedule changes

---
+++ AutoOpener1.java
@@ -0,0 +1,56 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+class AutoOpener1 implements Runnable {
+    public void run() {
+
+        Sleeper sleep = new Sleeper();
+        Opener open = new Opener();
+        GUI gui = new GUI();
+        SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
+        SimpleDateFormat dayformat = new SimpleDateFormat("E");
+
+        while (!Thread.currentThread().isInterrupted()) {
+            gui.setAuto1("Running...");
+            Date now = new Date();
+            String time = timeformat.format(now);
+            String day = dayformat.format(now);
+            if (day.equals("Mon") || day.equals("Wed") || day.equals("Fri")) {
+                if (time.equals("07:52:30")) {
+                    open.first();
+                    sleep.wait(3f);
+                }
+                if (time.equals("09:24:30")) {
+                    open.second();
+                    sleep.wait(3f);
+                }
+                if (time.equals("10:54:30")) {
+                    open.third();
+                    sleep.wait(3f);
+                }
+            }
+            if (day.equals("Tue") || day.equals("Thu")) {
+                if (time.equals("07:52:30")) {
+                    open.fourth();
+                    sleep.wait(3f);
+                }
+                if (time.equals("09:24:30")) {
+                    open.fifth();
+                    sleep.wait(3f);
+                }
+                if (time.equals("10:54:30")) {
+                    open.sixth();
+                    sleep.wait(3f);
+                }
+            }
+            if (day.equals("Mon")) {
+                if (time.equals("12:59:30")) {
+                    open.advisory();
+                    sleep.wait(3f);
+                }
+            }
+            sleep.wait(0.5f);
+        }
+        gui.setAuto1("Auto Open");
+    }   
+}
--- GUI.java
+++ GUI.java
@@ -6,7 +6,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 class GUI {
-    static JButton auto;
+    static JButton auto1;
+    static JButton auto2;
     static Thread t;
 
     public static void main(String[] args) {
@@ -15,67 +16,73 @@ class GUI {
         JPanel panel = new JPanel();
         frame.getContentPane().add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 313);
+        frame.setSize(500, 350);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         panel.setLayout(null);
 
         JLabel title = new JLabel("Automatic Zoom Opener");
-        title.setBounds(248-112, 48-13, 224, 26);
+        title.setBounds(136, 35, 224, 26);
         Font font = new Font("Arial", Font.BOLD, 18);
         title.setFont(font);
         panel.add(title);
 
         JButton first = new JButton("First");
-        first.setBounds(99-45, 96, 90, 26);
+        first.setBounds(54, 96, 90, 26);
         first.setFocusPainted(false);
         first.setBackground(Color.decode("#dedede"));
         panel.add(first);
 
         JButton second = new JButton("Second");
-        second.setBounds(198-45, 96, 90, 26);
+        second.setBounds(153, 96, 90, 26);
         second.setFocusPainted(false);
         second.setBackground(Color.decode("#dedede"));
         panel.add(second);
 
         JButton third = new JButton("Third");
-        third.setBounds(297-45, 96, 90, 26);
+        third.setBounds(252, 96, 90, 26);
         third.setFocusPainted(false);
         third.setBackground(Color.decode("#dedede"));
         panel.add(third);
 
         JButton fourth = new JButton("Fourth");
-        fourth.setBounds(396-45, 96, 90, 26);
+        fourth.setBounds(351, 96, 90, 26);
         fourth.setFocusPainted(false);
         fourth.setBackground(Color.decode("#dedede"));
         panel.add(fourth);
 
         JButton fifth = new JButton("Fifth");
-        fifth.setBounds(149-45, 130, 90, 26);
+        fifth.setBounds(104, 130, 90, 26);
         fifth.setFocusPainted(false);
         fifth.setBackground(Color.decode("#dedede"));
         panel.add(fifth);
 
         JButton sixth = new JButton("Sixth");
-        sixth.setBounds(248-45, 130, 90, 26);
+        sixth.setBounds(203, 130, 90, 26);
         sixth.setFocusPainted(false);
         sixth.setBackground(Color.decode("#dedede"));
         panel.add(sixth);
 
         JButton advis = new JButton("Advisory");
-        advis.setBounds(347-45, 130, 90, 26);
+        advis.setBounds(302, 130, 90, 26);
         advis.setFocusPainted(false);
         advis.setBackground(Color.decode("#dedede"));
         panel.add(advis);
 
-        auto = new JButton("Auto Open");
-        auto.setBounds(248-55, 192, 110, 29);
-        auto.setFocusPainted(false);
-        auto.setBackground(Color.decode("#dedede"));
-        panel.add(auto);
+        auto1 = new JButton("Auto Open 1");
+        auto1.setBounds(193, 192, 110, 29);
+        auto1.setFocusPainted(false);
+        auto1.setBackground(Color.decode("#dedede"));
+        panel.add(auto1);
+
+        auto2 = new JButton("Auto Open 2");
+        auto2.setBounds(193, 229, 110, 29);
+        auto2.setFocusPainted(false);
+        auto2.setBackground(Color.decode("#dedede"));
+        panel.add(auto2);
 
         JButton cancel = new JButton("Cancel");
-        cancel.setBounds(248-40, 229, 80, 29);
+        cancel.setBounds(208, 266, 80, 29);
         cancel.setFocusPainted(false);
         cancel.setBackground(Color.decode("#dedede"));
         panel.add(cancel);
@@ -122,9 +129,15 @@ class GUI {
                 act.advisory();
             }
         });
-        auto.addActionListener(new ActionListener(){
+        auto1.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                t = new Thread(new AutoOpener());
+                t = new Thread(new AutoOpener1());
+                t.start();
+            }
+        });
+        auto2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                t = new Thread(new AutoOpener2());
                 t.start();
             }
         });
@@ -136,8 +149,12 @@ class GUI {
         frame.setVisible(true);
     }
 
-    public void setAuto(String title) {
-        auto.setText(title);
+    public void setAuto1(String title) {
+        auto1.setText(title);
+    }
+
+    public void setAuto2(String title) {
+        auto2.setText(title);
     }
 
 }
